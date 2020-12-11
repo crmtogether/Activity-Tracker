e73e284c56df72f6a34d4f675dd554f7ef2323fb
marc@crmtogether.com <marc@crmtogether.com>
2020-12-11 13:07:47 +0000

    it now connects to the export to file from the search screens and records the search screen criteria

--- src/act_tracker/js/custom/act_tracker.js
+++ src/act_tracker/js/custom/act_tracker.js
@@ -24,14 +24,50 @@ function ExportToFileClicked(e)
 	var groupName=document.getElementById("_HIDDENReportName").value;
 	_logUserActivity("Export to File", groupName);
 }
+function getSearchFields()
+{
+	var res="";
+	var fields=document.getElementsByClassName('EDIT');
+	for (var i=0;i<fields.length;i++)
+	{
+		if ((fields[i]!=null)&&(fields[i].value!=""))
+		{
+			var _capt=document.getElementById("_Capt"+fields[i]	.id);
+			if (_capt!=null)
+			{
+				if (fields[i].tagName=="SELECT")
+				{
+					var _displayvalue=crm.getTrans(fields[i].id,fields[i].value);
+					if (fields[i].value=="sagecrm_code_all")
+						_displayvalue="All";
+					res+=_capt.innerText+ " "+ _displayvalue +'\n';
+				}else{
+					res+=_capt.innerText+ " "+ fields[i].value+'\n';
+				}
+
+			}
+		}
+	}
+	return res;
+}
 
 crm.ready(function() {
-	
     var _exportbtn=document.getElementById("Button_Export");
 	if (_exportbtn!=null)
 	{
 		var _strhref=new String(_exportbtn.href);
-		_strhref=_strhref.replace("document.EntryForm.submit();","ExportToFileClicked();document.EntryForm.submit();");
-		_exportbtn.href=_strhref;
+		var _stronclick=new String(_exportbtn.getAttribute("onclick"));
+		if (_stronclick.indexOf(".submit")>0)
+		{
+			var _searchEntity=SELECTMenuOption.options[SELECTMenuOption.selectedIndex].text;
+			_exportbtn.setAttribute("onclick","javascript:_logUserActivity('Export to File: "+_searchEntity+"',getSearchFields());"+_stronclick);
+		}else{
+			_strhref=_strhref.replace("document.EntryForm.submit();","ExportToFileClicked();document.EntryForm.submit();");
+			_exportbtn.href=_strhref;
+		}
+		console.log('act_tracker button found');	
+	}else{
+		console.log('act_tracker button NOT found');
 	}
+
 });
